f4655349bfb744c0073a3950f1d11687fa7e168b
Zack Traczyk <me@zacktraczyk.com>
2023-07-14 14:18:47 -0700

    map-demo loading

--- src/app/(dashboard)/listings-demo/listings.tsx
+++ src/app/(dashboard)/listings-demo/listings.tsx
@@ -3,7 +3,42 @@ import { Feature, Point, GeoJsonProperties } from 'geojson'
 import { useMap } from 'react-map-gl'
 
 export default function Listings() {
-    const { data: listings } = useListings()
+    const { data: listings, isLoading, isError } = useListings()
+
+    if (isLoading) {
+        return (
+            <div className='flex h-60 flex-col items-center gap-5 overflow-y-scroll md:h-full'>
+                <h1 className='w-full'>Listings</h1>
+                <div className='flex w-full animate-pulse items-center justify-center rounded-lg border-2 p-3 hover:bg-gray-100'>
+                    <p className='h-5 w-20 rounded bg-gray-200 text-center'></p>
+                </div>
+                <div className='flex w-full animate-pulse items-center justify-center rounded-lg border-2 p-3 hover:bg-gray-100'>
+                    <p className='h-5 w-20 rounded bg-gray-200 text-center'></p>
+                </div>
+                <div className='flex w-full animate-pulse items-center justify-center rounded-lg border-2 p-3 hover:bg-gray-100'>
+                    <p className='h-5 w-20 rounded bg-gray-200 text-center'></p>
+                </div>
+                <div className='flex w-full animate-pulse items-center justify-center rounded-lg border-2 p-3 hover:bg-gray-100'>
+                    <p className='h-5 w-20 rounded bg-gray-200 text-center'></p>
+                </div>
+                <div className='flex w-full animate-pulse items-center justify-center rounded-lg border-2 p-3 hover:bg-gray-100'>
+                    <p className='h-5 w-20 rounded bg-gray-200 text-center'></p>
+                </div>
+                <div className='flex w-full animate-pulse items-center justify-center rounded-lg border-2 p-3 hover:bg-gray-100'>
+                    <p className='h-5 w-20 rounded bg-gray-200 text-center'></p>
+                </div>
+            </div>
+        )
+    }
+
+    if (isError) {
+        return (
+            <div className='flex h-60 flex-col items-center gap-5 overflow-y-scroll md:h-full'>
+                <h1 className='w-full'>Listings</h1>
+                <h2 className='text-red-500'>Sorry an error occurred :/</h2>
+            </div>
+        )
+    }
 
     return (
         <div className='flex h-60 flex-col items-center gap-5 overflow-y-scroll md:h-full'>
@@ -27,7 +62,6 @@ function Listing(props: PropListing) {
     const color = listing.properties?.color || '#FFFFFF'
 
     const handleClick = () => {
-        console
         listingsMap?.flyTo({
             center: listing.geometry.coordinates as [number, number],
         })
