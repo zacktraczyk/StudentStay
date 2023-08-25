730d95426e5a295df107dbe53fe353ff3cb790a1
Zack Traczyk <20495250+xxzbuckxx@users.noreply.github.com>
2023-08-25 15:34:24 -0700

    fix server function error

--- nextjs/src/app/(site)/(public-profile)/u/[profileId]/page.tsx
+++ nextjs/src/app/(site)/(public-profile)/u/[profileId]/page.tsx
@@ -6,9 +6,10 @@ export default async function PublicProfilePage({
 }: {
   params: { profileId: string }
 }) {
-  // Created subcomoonent to allow future route interception
+  // Created subcomponent to allow future route interception
   return (
     <div className='mx-auto max-w-7xl flex-col items-center justify-between gap-x-6 p-6 sm:px-8'>
+      {/* @ts-expect-error Server Component */}
       <PublicProfile profile_id={profileId ?? ''} />
     </div>
   )
